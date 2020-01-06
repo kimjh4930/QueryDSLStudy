6b59d766ac2eaa274dc9c3c8e4e513da46948a07
Kim, Junha <kimjh4930@gmail.com>
2020-01-06 22:36:49 +0900

    QueryDSL Gradle Setting

---
+++ src/main/java/com/example/querydsl/QueryDslStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryDslStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryDslStudyApplication.class, args);
+    }
+
+}
